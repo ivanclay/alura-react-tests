9fc8a525ae6bdfcfebfb9b65542d79ceeca9d1e6
ivanclay <ivan.clay.moura@gmail.com>
2021-11-01 11:43:35 -0300

    --

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Conta from "./Conta";
 
 describe('Conta Component', () => {
@@ -7,5 +7,12 @@ describe('Conta Component', () => {
         render(<Conta saldo={ 1000 } />);
         const saldo = screen.getByTestId('saldo-conta');
         expect(saldo.textContent).toBe('R$ 1000');
+    });
+
+    it('If buttom click, called function', () => {
+        const funcaoRealizarTransacao = jest.fn();
+        render(<Conta saldo={ 1000 } realizarTransacao={funcaoRealizarTransacao} />);
+        fireEvent.click(screen.getByText('Realizar operação'));
+        expect(funcaoRealizarTransacao).toHaveBeenCalled();
     });
 })
